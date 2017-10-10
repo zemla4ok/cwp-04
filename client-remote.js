1ac3dbeb0715455f898ebda8e776eda9407743e7
zemla4ok <dima-vital@live.com>
2017-10-10 23:32:40 +0300

    client can send DECODE-string

--- client-remote.js
+++ client-remote.js
@@ -22,6 +22,7 @@ client.on('data', (data) => {
     else if(data === goodResp){
         client.write('COPY C:\\Users\\Dmitry\\Desktop\\cw4\\original.txt C:\\Users\\Dmitry\\Desktop\\cw4\\copy.txt');
         client.write('ENCODE C:\\Users\\Dmitry\\Desktop\\cw4\\original.txt C:\\Users\\Dmitry\\Desktop\\cw4\\encode.txt 123');
+        client.write('DECODE C:\\Users\\Dmitry\\Desktop\\cw4\\encode.txt C:\\Users\\Dmitry\\Desktop\\cw4\\decode.txt 123');
     }
 });
 
